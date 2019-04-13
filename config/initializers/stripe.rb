2aa35ff281895d714b25d79236d3cdaf9d49b581
Erwin <35572058+erwinleonardy@users.noreply.github.com>
2019-04-13 10:38:52 +0800

    Fix security issue

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_live_32cC2OUaORT6Sgitt1ltPeEA1'
-  :secret_key => 'sk_live_OaASXG1s0xABCp0k5d8Asbal'
+  :publishable_key => ENV['PUBLISHABLE_KEY']
+  :secret_key => ENV['SECRET_KEY']
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
